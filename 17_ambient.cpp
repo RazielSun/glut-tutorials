dc8a454fdf6dbdd43468187003d339a6c6817869
Alexandr Kardakov <razielsun@gmail.com>
2016-10-16 19:37:07 +0300

    add ambient light

---
+++ 17_ambient.cpp
@@ -0,0 +1,279 @@
+
+#include <assert.h>
+#include <math.h>
+#include "utils/util_3d.h"
+#include "utils/util_camera.h"
+#include "utils/util_pipeline.h"
+#include "utils/util_texture.h"
+#include <iostream>
+
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 768
+
+const char* WINDOW_NAME = "Tutorial 16";
+
+GLuint ibo;
+GLuint vbo;
+
+// UNIFORMS FOR SHADERS
+GLuint uWPM;
+GLuint uSampler;
+GLuint uAmbientColor;
+GLuint uAmbientIntensity;
+
+Camera *camera = NULL;
+Texture *texture = NULL;
+
+static std::string vertex_shader = ""
+"attribute vec3 position;"
+"attribute vec2 texCoord;"
+"uniform mat4 WPM;"
+"varying vec2 uvCoord;"
+"void main()"
+"{"
+"   gl_Position = WPM * vec4(position, 1.0);"
+"	uvCoord = texCoord;"
+"}";
+
+static std::string fragment_shader = ""
+"uniform sampler2D sampler;"
+"uniform vec3 ambientColor;"
+"uniform float ambientIntensity;"
+"varying vec2 uvCoord;"
+"void main()"
+"{"
+"    gl_FragColor = texture2D(sampler, uvCoord) * vec4(ambientColor, 1.0) * ambientIntensity;"
+"}";
+
+static float directionLightIntensity = 0.5f;
+
+void render ()
+{
+	camera->OnRender();
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	static float scale = 0.0f;
+	scale += 0.1f;
+
+	Pipeline p;
+
+	p.Rotate(0.0f, scale, 0.0f);
+	p.Pos(0.0f, 0.0f, 3.0f);
+	p.SetPerspectiveProj(60.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 1.0f, 100.0f);
+
+	p.SetCamera(*camera);
+
+	glUniform1f(uAmbientIntensity, directionLightIntensity);
+	glUniform3f(uAmbientColor, 1.0f, 1.0f, 1.0f);
+	glUniformMatrix4fv(uWPM, 1, GL_TRUE, (const GLfloat*)p.GetTrans());
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)12);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+	texture->Bind(GL_TEXTURE0);
+	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+}
+
+void createContext ()
+{
+	Vertex vertices[4] = { Vertex(Vector3f(-1.0f, -1.0f, 0.5773f), Vector2f(0.0f, 0.0f)),
+                           Vertex(Vector3f(0.0f, -1.0f, -1.15475f), Vector2f(0.5f, 0.0f)),
+                           Vertex(Vector3f(1.0f, -1.0f, 0.5773f),  Vector2f(1.0f, 0.0f)),
+                           Vertex(Vector3f(0.0f, 1.0f, 0.0f),      Vector2f(0.5f, 1.0f)) };
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int indices[] = { 0, 3, 1,
+                               1, 3, 2,
+                               2, 3, 0,
+                               0, 1, 2 };
+
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+void addShader (GLuint program, const char* shaderText, GLenum shaderType)
+{
+	GLuint shader = glCreateShader(shaderType);
+
+	glShaderSource(shader, 1, &shaderText, NULL);
+    glCompileShader(shader);
+
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(shader, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", shaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(program, shader);
+}
+
+void createShaderProgram ()
+{
+	GLuint program = glCreateProgram();
+
+	addShader(program, vertex_shader.c_str(), GL_VERTEX_SHADER);
+	addShader(program, fragment_shader.c_str(), GL_FRAGMENT_SHADER);
+
+	GLint success = 0;
+
+	glBindAttribLocation(program, 0, "position");
+    glBindAttribLocation(program, 1, "texCoord");
+
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+    GLchar ErrorLog[1024] = { 0 };
+
+	if (success == 0)
+	{
+		glGetProgramInfoLog(program, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+	}
+
+    glValidateProgram(program);
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &success);
+
+    if (!success) 
+   	{
+        glGetProgramInfoLog(program, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(program);
+
+   
+
+    uWPM = glGetUniformLocation(program, "WPM");
+	assert(uWPM != 0xFFFFFFFF);
+
+	uSampler = glGetUniformLocation(program, "sampler");
+	assert(uSampler != 0xFFFFFFFF);
+
+	uAmbientColor = glGetUniformLocation(program, "ambientColor");
+	assert(uAmbientColor != 0xFFFFFFFF);
+
+	uAmbientIntensity = glGetUniformLocation(program, "ambientIntensity");
+	assert(uAmbientIntensity != 0xFFFFFFFF);
+}
+
+SDL_Window *window;
+
+void init()
+{
+	if ( SDL_Init(SDL_INIT_VIDEO) < 0 )
+	{
+		fprintf(stderr, "Unable to init SDL, error: '%s'\n", SDL_GetError());
+		exit(1);
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 6);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+
+	window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+	if (window == NULL)
+	{
+		fprintf(stderr, "Unable to create SDL Context");
+		exit(1);
+	}
+
+	glewExperimental = GL_TRUE;
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+	    fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+	    exit(1);
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	init();
+
+	camera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
+    glEnable(GL_CULL_FACE);
+
+	createContext();
+	createShaderProgram();
+    glUniform1i(uSampler, 0);
+
+	texture = new Texture(GL_TEXTURE_2D, "content/uvLayoutGrid.jpg");
+	if (!texture->Load()) {
+		return 1;
+	}
+
+	bool running = true;
+
+	while(running)
+	{
+		SDL_Event event;
+
+		while ( SDL_PollEvent(&event) )
+		{
+			switch(event.type)
+			{
+				case SDL_QUIT:
+					running = false;
+					break;
+				case SDL_MOUSEMOTION:
+					camera->OnMouse(event.motion.x, event.motion.y);
+					break;
+
+				case SDL_KEYDOWN:
+				{
+					camera->OnKeyboard(event.key.keysym.sym);
+					switch(event.key.keysym.sym)
+					{
+						case SDLK_s:
+							directionLightIntensity += 0.1f;
+							break;
+						case SDLK_a:
+							directionLightIntensity -= 0.1f;
+							break;
+						case SDLK_ESCAPE:
+							running = false;
+							break;
+					}
+				}
+				break;
+			}
+		}
+		
+		render();
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_Quit();
+	return 0;
+}
